4381b9a9931d2d6c25a873257fd8f3ea0c49c58b
Vijay Dev <vijaydev.in@gmail.com>
2017-01-22 13:00:15 +0530

    proper names and examples

--- src/ngAugmentNativeScroll.js
+++ src/ngAugmentNativeScroll.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('ngNativeScroll')
+angular.module('ngAugmentNativeScroll')
     .directive('connectScrolls', function () {
         return {
             restrict: 'E',
