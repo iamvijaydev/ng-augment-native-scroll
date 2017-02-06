326ded3396a1b0f2b23a91071e5fe85b3e9c0a38
Vijay Dev <vijaydev.in@gmail.com>
2017-02-07 00:48:11 +0530

    fixed proper UMD return

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
+var moduleName = 'ng-augment-native-scroll';
 var angular = require('angular');
 
-angular.module('ng-augment-native-scroll', [])
+angular.module(moduleName, [])
     .factory('augNsUtils', require('./augNsUtils.factory.js'))
     .value('augNsOptions', require('./augNsOptions.value.js'))
     .value('kineticEngine', require('./kineticEngine.value.js'))
     .directive('connectScrolls', require('./connectScrolls.directive.js'))
     .directive('scrollArea', require('./scrollArea.directive.js'))
     .directive('kineticScroll', require('./kineticScroll.directive.js'));
+
+return moduleName;
